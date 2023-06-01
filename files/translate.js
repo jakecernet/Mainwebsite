b88cc2275209c9e3b10e451b94d0a04d41c87d7d
Jaka Černetič <117542696+jakecernet@users.noreply.github.com>
2023-06-01 19:37:55 +0200

    website optimizations

--- files/translate.js
+++ files/translate.js
@@ -7,7 +7,7 @@ function changeToSlo() {
     document.getElementById("file").innerHTML = "Projekti";
     document.getElementById("about-text").innerHTML = "O meni";
     document.getElementById("cont").innerHTML = "Kontakt";
-    document.getElementById("us").innerHTML = "Oj! Sem Jaka, dijak 1. letnika Vegove gimnazije. Trenutno se učim izdelavo spletnih strani, programiranje v JavaScriptu, batchu in C++.";
+    document.getElementById("us").innerHTML = "Oj! Sem Jaka Černetič, dijak 1. letnika Vegove gimnazije. Trenutno se učim izdelavo spletnih strani, programiranje v JavaScriptu, batchu in C++.";
     document.getElementById("lok").innerHTML = "Tukaj so povezave za moja socialna omrežja.";
     document.getElementById("projects-title").innerHTML = "Projekti";
     document.getElementById("proj").innerHTML = "Tukaj so nekateri moji projekti.";
@@ -28,7 +28,7 @@ function changeToEng() {
     document.getElementById("file").innerHTML = "Projects";
     document.getElementById("about-text").innerHTML = "About me";
     document.getElementById("cont").innerHTML = "Contact";
-    document.getElementById("us").innerHTML = "Hi! I'm Jaka. I'm a student of the first year at Vegova high school. I am currently learning web development, JavaScript, batch and C++.";
+    document.getElementById("us").innerHTML = "Hi! I'm Jaka Černetič. I'm a student of the first year at Vegova high school. I am currently learning web development, JavaScript, batch and C++.";
     document.getElementById("lok").innerHTML = "Here are some of my social media links.";
     document.getElementById("projects-title").innerHTML = "Projects";
     document.getElementById("proj").innerHTML = "Here are some of my projects.";
