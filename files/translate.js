fd1cf93f6ba5263acf4b7e98a75e5da9145b6776
jakecernet <117542696+jakecernet@users.noreply.github.com>
2023-06-02 16:55:31 +0200

    design changes

--- files/translate.js
+++ files/translate.js
@@ -5,9 +5,9 @@ function changeToSlo() {
     document.getElementById("about").innerHTML = "O meni";
     document.getElementById("kont").innerHTML = "Kontakt";
     document.getElementById("file").innerHTML = "Projekti";
-    document.getElementById("about-text").innerHTML = "O meni";
     document.getElementById("cont").innerHTML = "Kontakt";
-    document.getElementById("us").innerHTML = "Oj! Sem Jaka Černetič, dijak 1. letnika Vegove gimnazije. Trenutno se učim izdelavo spletnih strani, programiranje v JavaScriptu, batchu in C++.";
+    document.getElementById("us").innerHTML = "Oj! Sem Jaka Černetič.";
+    document.getElementById("us-h2").innerHTML = "Sem dijak prvega letnika Vegove gimnazije. Trenutno se učim izdelavo spletnih strani, JavaScript, batch in C++.";
     document.getElementById("lok").innerHTML = "Tukaj so povezave za moja socialna omrežja.";
     document.getElementById("projects-title").innerHTML = "Projekti";
     document.getElementById("proj").innerHTML = "Tukaj so nekateri moji projekti.";
@@ -26,9 +26,9 @@ function changeToEng() {
     document.getElementById("about").innerHTML = "About";
     document.getElementById("kont").innerHTML = "Contact";
     document.getElementById("file").innerHTML = "Projects";
-    document.getElementById("about-text").innerHTML = "About me";
     document.getElementById("cont").innerHTML = "Contact";
-    document.getElementById("us").innerHTML = "Hi! I'm Jaka Černetič. I'm a student of the first year at Vegova high school. I am currently learning web development, JavaScript, batch and C++.";
+    document.getElementById("us").innerHTML = "Hi! I'm Jaka Černetič.";
+    document.getElementById("us-h2").innerHTML = "I'm a student in the first grade at Vegova high school. I am currently learning web development, JavaScript, batch and C++.";
     document.getElementById("lok").innerHTML = "Here are some of my social media links.";
     document.getElementById("projects-title").innerHTML = "Projects";
     document.getElementById("proj").innerHTML = "Here are some of my projects.";
