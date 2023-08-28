f51e9f9f826ca40e7c5f3cf223ddefad0fcd6df0
Jaka Černetič <jaka.cernetic@students.vegova.si>
2023-08-28 18:59:14 +0200

    new website

---
+++ files/index.js
@@ -0,0 +1,29 @@
+window.addEventListener('scroll', function () {
+    var scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+
+    var scrollToTop = document.querySelector('.top');
+    if (scrollPos > 400) {
+        scrollToTop.style.display = 'flex';
+    } else {
+        scrollToTop.style.display = 'none';
+    }
+});
+
+function showNav() {
+    var nav = document.querySelector('.nav');
+    if (nav.style.opacity == 1) {
+        nav.style.opacity = 0;
+        nav.style.pointerEvents = 'none';
+    } else {
+        nav.style.opacity = 1;
+        nav.style.pointerEvents = 'auto';
+    }
+}
+
+function switchTheme() {
+    switcher = document.querySelector('.switcher');
+    document.body.classList.toggle('dark');
+    document.body.classList.toggle('light');
+    switcher.classList.toggle('fa-moon');
+    switcher.classList.toggle('fa-sun');
+}
--- files/translate.js
+++ files/translate.js
@@ -2,51 +2,54 @@ var languageSelect = document.getElementById("language-select");
 var selectedLanguage = languageSelect.value;
 
 function changeToSlo() {
-    document.getElementById("about").innerHTML = "O meni";
-    document.getElementById("file").innerHTML = "Projekti";
-    document.getElementById("contact").innerHTML = "Kontakt";
-    document.getElementById("us").innerHTML = "Oj! Sem Jaka Černetič.";
-    document.getElementById("us-h2").innerHTML = "Sem dijak prvega letnika Vegove gimnazije. Trenutno se učim izdelavo spletnih strani, JavaScript, batch in C++.";
-    document.getElementById("projects-title").innerHTML = "Projekti";
-    document.getElementById("proj").innerHTML = "Tukaj so nekateri moji projekti.";
-    document.getElementById("proj1").innerHTML = "Moja osebna spletna stran.";
-    document.getElementById("proj2").innerHTML = "Izboljšava spletne strani za naročanje hrane.";
-    document.getElementById("proj3").innerHTML = "Spletna stran z mojimi igrami, narejenimi v JavaScriptu.";
-    document.getElementById("proj4").innerHTML = "Pretvornik iz YouTuba v MP3, napisan v Pythonu.";
-    document.getElementById("proj5").innerHTML = "Preprosta spletna beležka.";
-    document.getElementById("proj6").innerHTML = "Spletna stran za naš razred.";
-    document.getElementById("cont-me").innerHTML = "Kontakt";
-    document.getElementById("cont-me-h2").innerHTML = "Če me želite kontaktirati, lahko to storite tukaj.";
-    document.getElementById("timeline").innerHTML = "Časovnica";
-    document.getElementById("13").innerHTML = "Začel osnovno šolo.";
-    document.getElementById("20").innerHTML = "Začel svoje programersko potovanje.";
-    document.getElementById("22-1").innerHTML = "Začel srednjo šolo.";
-    document.getElementById("22-2").innerHTML = "Začel se aktivno učiti izdelave spletnih strani.";
-    document.getElementById("now").innerHTML = "Učenje izdelave spletnih strani, JavaScripta, batcha in C++.";
+    //NavBar
+    document.getElementById("home").innerHTML = "Domov";
+    document.getElementById("nav-about").innerHTML = "O meni";
+    document.getElementById("nav-contact").innerHTML = "Kontakt";
+    document.getElementById("nav-skills").innerHTML = "Znanja";
+    document.getElementById("nav-projects").innerHTML = "Projekti";
+
+    //Welcome text
+    document.getElementById("welcome-text").innerHTML = "Dijak, oblikovalec spletnih strani, računalniški navdušenec";
+
+    //About me
+    document.getElementById("about-text").innerHTML = "Sem dijak 2. letnika programa Tehniška gimnazija na Vegovi Ljubljana. V prostem času se ukvarjam s programiranjem in kolesarjenjem.";
+
+
+    //Timeline
+    document.getElementById("13").innerHTML = "Začel hoditi v osnovno šolo";
+    document.getElementById("20").innerHTML = "Začel svojo programersko pot";
+    document.getElementById("22-1").innerHTML = "Začel hoditi v srednjo šolo";
+    document.getElementById("22-2").innerHTML = "Začel aktivno učenje spletnega oblikovanja";
+    document.getElementById("now").innerHTML = "Učim se spletnega oblikovanja, JavaScripta, batcha in C++";
+
+    //Skills
+    document.getElementById("skills-title").innerHTML = "Znanja";
 }
 
 function changeToEng() {
-    document.getElementById("about").innerHTML = "About me";
-    document.getElementById("file").innerHTML = "Projects";
-    document.getElementById("contact").innerHTML = "Contact me";
-    document.getElementById("us").innerHTML = "Hi! I'm Jaka Černetič.";
-    document.getElementById("us-h2").innerHTML = "I'm a student in the first grade at Vegova high school. I am currently learning web development, JavaScript, batch and C++.";
-    document.getElementById("projects-title").innerHTML = "Projects";
-    document.getElementById("proj").innerHTML = "Here are some of my projects.";
-    document.getElementById("proj1").innerHTML = "My personal website.";
-    document.getElementById("proj2").innerHTML = "Food ordering service improvement.";
-    document.getElementById("proj3").innerHTML = "Website with my games, made in JavaScript.";
-    document.getElementById("proj4").innerHTML = "YouTube to MP3 converter, made in Python.";
-    document.getElementById("proj5").innerHTML = "Simple notes taking website.";
-    document.getElementById("proj6").innerHTML = "A website for our class.";
-    document.getElementById("cont-me").innerHTML = "Contact me";
-    document.getElementById("cont-me-h2").innerHTML = "If you want to contact me, you can do it here.";
-    document.getElementById("timeline").innerHTML = "Timeline";
-    document.getElementById("13").innerHTML = "Started primary school.";
-    document.getElementById("20").innerHTML = "Started my coding journey.";
-    document.getElementById("22-1").innerHTML = "Started high school.";
-    document.getElementById("22-2").innerHTML = "Started actively learning web development.";
-    document.getElementById("now").innerHTML = "Learning web development, JavaScript, batch and C++.";
+    //NavBar
+    document.getElementById("home").innerHTML = "Home";
+    document.getElementById("nav-about").innerHTML = "About";
+    document.getElementById("nav-contact").innerHTML = "Contact";
+    document.getElementById("nav-skills").innerHTML = "Skills";
+    document.getElementById("nav-projects").innerHTML = "Projects";
+
+    //Welcome text
+    document.getElementById("welcome-text").innerHTML = "Student, web developer, computer enthusiast";
+
+    //About me
+    document.getElementById("about-text").innerHTML = "I'm a student of the 2nd year of the Technical Gymnasium program at Vegova Ljubljana. In my free time I like to program and ride my bike.";
+
+    //Timeline
+    document.getElementById("13").innerHTML = "Started primary school";
+    document.getElementById("20").innerHTML = "Started my coding journey";
+    document.getElementById("22-1").innerHTML = "Started high school";
+    document.getElementById("22-2").innerHTML = "Started actively learning web development";
+    document.getElementById("now").innerHTML = "Learning web development, JavaScript, batch and C++";
+
+    //Skills
+    document.getElementById("skills-title").innerHTML = "Skills";
 }
 
 function translatePage(selectedLanguage) {
