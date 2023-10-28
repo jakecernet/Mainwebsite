4958a14b43cefdaf06fc936615eff50a028b90d1
Jaka Černetič <jaka.cernetic@students.vegova.si>
2023-10-28 23:15:35 +0200

    yessss slider finishan

--- files/slider.js
+++ files/slider.js
@@ -1,58 +1,69 @@
 n = 1;
 
 //show only first slide, hide the rest
-for (i = 2; i <= 5; i++) {
+for (i = 2; i <= 9; i++) {
     document.getElementById(i).style.display = "none";
 }
 
 function moveSlidesLeft() {
     n++;
-    document.getElementById(n - 1).style.animation = "slideOut 1s";
-    document.getElementById(n).style.animation = "slideIn 1s";
 
-    //transition
-    setTimeout(function () {
-        document.getElementById(n - 1).style.animation = "";
-        document.getElementById(n - 1).style.display = "none";
+    //if n is more than 4, go back to 1
+    if (n > 9) {
+        n = 1;
+        document.getElementById(n).style.animation = "slideInLeft 0.5s";
         document.getElementById(n).style.display = "block";
-        document.getElementById(n).style.animation = "";
-    }, 1000);
+        document.getElementById(9).style.display = "none";
+        document.getElementById(9).style.animation = "";
+    } else if (n < 1) {
+        n = 9;
+        document.getElementById(n).style.animation = "slideInLeft 0.5s";
+        document.getElementById(n).style.display = "block";
+        document.getElementById(1).style.display = "none";
+        document.getElementById(1).style.animation = "";
+    } else {
+        document.getElementById(n - 1).style.animation = "slideOutLeft 0.5s";
+        document.getElementById(n).style.animation = "slideInLeft 0.5s";
 
-    console.log(n);
+        //transition
+        setTimeout(function () {
+            document.getElementById(n - 1).style.animation = "";
+            document.getElementById(n - 1).style.display = "none";
+            document.getElementById(n).style.display = "block";
+            document.getElementById(n).style.animation = "";
+        }, 500);
 
-    //if n is 5, then hide right arrow
-    if (n == 5) {
-        document.querySelector(".right-arrow").style.display = "none";
-    }
-
-    //if n is not 5, then show right arrow
-    if (n != 5) {
-        document.querySelector(".right-arrow").style.display = "flex";
+        console.log(n);
     }
 }
 
 function moveSlidesRight() {
     n--;
-    document.getElementById(n + 1).animation = "slideOut 1s";
-    document.getElementById(n).style.animation = "slideIn 1s";
 
-    //transition
-    setTimeout(function () {
-        document.getElementById(n + 1).style.animation = "";
-        document.getElementById(n + 1).style.display = "none";
+    if (n > 9) {
+        n = 1;
+        document.getElementById(n).style.animation = "slideInRight 0.5s";
         document.getElementById(n).style.display = "block";
-        document.getElementById(n).style.animation = "";
-    }, 1000);
+        document.getElementById(9).style.display = "none";
+        document.getElementById(9).style.animation = "";
+    } else if (n < 1) {
+        n = 9;
+        document.getElementById(n).style.animation = "slideInRight 0.5s";
+        document.getElementById(n).style.display = "block";
+        document.getElementById(1).style.display = "none";
+        document.getElementById(1).style.animation = "";
+    } else {
+        document.getElementById(n + 1).style.animation = "slideOutRight 0.5s";
+        document.getElementById(n).style.animation = "slideInRight 0.5s";
 
-    console.log(n);
+        //transition
+        setTimeout(function () {
+            document.getElementById(n + 1).style.animation = "";
+            document.getElementById(n + 1).style.display = "none";
+            document.getElementById(n).style.display = "block";
+            document.getElementById(n).style.animation = "";
+        }, 500);
 
-    //if n is 1, then hide left arrow
-    if (n == 1) {
-        document.querySelector(".left-arrow").style.display = "none";
-    }
-
-    //if n is not 1, then show left arrow
-    if (n != 1) {
-        document.querySelector(".left-arrow").style.display = "flex";
+        console.log(n);
     }
 }
