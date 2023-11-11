1e529dc2e06df6565d6240b2901628c481de36e6
Jaka Černetič <jaka.cernetic@students.vegova.si>
2023-11-11 22:41:14 +0100

    yes

--- files/slider.js
+++ files/slider.js
@@ -1,43 +1,92 @@
 let n = 1;
 
-// Initialize the slides
-for (let i = 2; i <= 10; i++) {
+for (let i = 1; i <= 10; i++) {
     document.getElementById(i).style.display = "none";
 }
 
+// Initialize the slides (show only the first two slides)
+document.getElementById(1).style.opacity = "1";
+document.getElementById(1).style.display = "block";
+document.getElementById(1).style.transform = "translateX(0%)";
+document.getElementById(2).style.opacity = "0.5";
+document.getElementById(2).style.display = "block";
+document.getElementById(2).style.transform = "translateX(20%)";
+document.getElementById(10).style.opacity = "0.5";
+document.getElementById(10).style.display = "block";
+document.getElementById(10).style.transform = "translateX(-20%)";
+
 function moveSlides(direction) {
-    if (direction === "left") {
-        n++;
-    } else if (direction === "right") {
-        n--;
+    n--;
+
+    let left = n;
+    let right = n + 2;
+
+    if (direction == "left") {
+        document.getElementById(2).style.transform = "translateX(0%)";
+        if (left < 1){
+            left = 9;
+            right = 1;
+        }
+        if (right > 10) right = 1;
+
+        let leftElement = document.getElementById(left);
+        let rightElement = document.getElementById(right + 1);
+
+        if (leftElement) {
+            leftElement.style.animation = "disappearLeft 1s";
+            leftElement.style.display = "none";
+            console.log("previous left slide disappeared");
+        }
+
+        document.getElementById(right).style.display = "block";
+        document.getElementById(right).style.animation = "slideInLeft 1s";
+        document.getElementById(right).style.opacity = "1";
+        document.getElementById(right).style.transform = "translateX(0%)";
+        console.log("previous right slide is now the current slide");
+
+        if (rightElement) {
+            rightElement.style.display = "block";
+            rightElement.style.animation = "appearRight 1s";
+            rightElement.style.opacity = "0.5";
+            rightElement.style.transform = "translateX(20%)";
+            console.log("next right slide appeared");
+        }
+
+        //lower the opacity of the previous current slide
+        document.getElementById(n + 1).style.animation = "slideOutLeft 1s";
+        document.getElementById(n + 1).style.opacity = "0.5";
+        document.getElementById(n + 1).style.transform = "translateX(-20%)";
+        console.log("previous current slide is now the left slide");
+
+        n = right;
+    } else if (direction == "right") {
+        if (left < 1) left = 10;
+        if (right > 10) right = 1;
+
+        let leftElement = document.getElementById(left - 1);
+        let rightElement = document.getElementById(right);
+
+        if (leftElement) {
+            leftElement.style.display = "block";
+            leftElement.style.animation = "appearLeft 1s";
+            leftElement.style.opacity = "0.5";
+            console.log("next left slide appeared");
+        }
+
+        if (rightElement) {
+            rightElement.style.animation = "disappearRight 1s";
+            rightElement.style.display = "none";
+            console.log("previous right slide disappeared");
+        }
+
+        document.getElementById(left).style.display = "block";
+        document.getElementById(left).style.animation = "slideInLeft 1s";
+        document.getElementById(left).style.opacity = "1";
+        console.log("previous left slide is now the current slide");
+
+        n = left;
     }
 
-    if (n > 10) {
-        n = 1;
-        document.getElementById(10).style.animation = "slideOutLeft 0.5s";
-        document.getElementById(10).style.display = "none";
-    } else if (n < 1) {
-        n = 10;
-        document.getElementById(1).style.animation = "slideOutRight 0.5s";
-        document.getElementById(1).style.display = "none";
-    }
-
-    const current = document.getElementById(n);
-    const next = direction === "left" ? document.getElementById(n - 1) : document.getElementById(n + 1);
-
-    current.style.animation = `slideIn${direction === "left" ? "Left" : "Right"} 0.5s`;
-    current.style.display = "block";
-
-    if (next) {
-        next.style.animation = `slideOut${direction === "left" ? "Left" : "Right"} 0.5s`;
-
-        // Transition
-        setTimeout(() => {
-            next.style.animation = "";
-            next.style.display = "none";
-            current.style.animation = "";
-        }, 500);
-    }
-
-    console.log(n);
+    if (n < 1) n = 10;
+    if (n > 10) n = 1;
 }
