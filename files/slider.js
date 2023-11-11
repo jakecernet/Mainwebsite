286d30fe2a8b42bba4808f0533a01459ea17345b
jakecernet <jaka.cernetic@students.vegova.si>
2023-11-11 12:39:12 +0100

    test

--- files/slider.js
+++ files/slider.js
@@ -1,69 +1,43 @@
-n = 1;
+let n = 1;
 
-//show only first slide, hide the rest
-for (i = 2; i <= 10; i++) {
+// Initialize the slides
+for (let i = 2; i <= 10; i++) {
     document.getElementById(i).style.display = "none";
 }
 
-function moveSlidesLeft() {
-    n++;
-
-    //if n is more than 4, go back to 1
-    if (n > 10) {
-        n = 1;
-        document.getElementById(n).style.animation = "slideInLeft 0.5s";
-        document.getElementById(n).style.display = "block";
-        document.getElementById(10).style.display = "none";
-        document.getElementById(10).style.animation = "";
-    } else if (n < 1) {
-        n = 10;
-        document.getElementById(n).style.animation = "slideInLeft 0.5s";
-        document.getElementById(n).style.display = "block";
-        document.getElementById(1).style.display = "none";
-        document.getElementById(1).style.animation = "";
-    } else {
-        document.getElementById(n - 1).style.animation = "slideOutLeft 0.5s";
-        document.getElementById(n).style.animation = "slideInLeft 0.5s";
-
-        //transition
-        setTimeout(function () {
-            document.getElementById(n - 1).style.animation = "";
-            document.getElementById(n - 1).style.display = "none";
-            document.getElementById(n).style.display = "block";
-            document.getElementById(n).style.animation = "";
-        }, 500);
-
-        console.log(n);
+function moveSlides(direction) {
+    if (direction === "left") {
+        n++;
+    } else if (direction === "right") {
+        n--;
     }
-}
-
-function moveSlidesRight() {
-    n--;
 
     if (n > 10) {
         n = 1;
-        document.getElementById(n).style.animation = "slideInRight 0.5s";
-        document.getElementById(n).style.display = "block";
+        document.getElementById(10).style.animation = "slideOutLeft 0.5s";
         document.getElementById(10).style.display = "none";
-        document.getElementById(10).style.animation = "";
     } else if (n < 1) {
         n = 10;
-        document.getElementById(n).style.animation = "slideInRight 0.5s";
-        document.getElementById(n).style.display = "block";
+        document.getElementById(1).style.animation = "slideOutRight 0.5s";
         document.getElementById(1).style.display = "none";
-        document.getElementById(1).style.animation = "";
-    } else {
-        document.getElementById(n + 1).style.animation = "slideOutRight 0.5s";
-        document.getElementById(n).style.animation = "slideInRight 0.5s";
-
-        //transition
-        setTimeout(function () {
-            document.getElementById(n + 1).style.animation = "";
-            document.getElementById(n + 1).style.display = "none";
-            document.getElementById(n).style.display = "block";
-            document.getElementById(n).style.animation = "";
-        }, 500);
-
-        console.log(n);
     }
+
+    const current = document.getElementById(n);
+    const next = direction === "left" ? document.getElementById(n - 1) : document.getElementById(n + 1);
+
+    current.style.animation = `slideIn${direction === "left" ? "Left" : "Right"} 0.5s`;
+    current.style.display = "block";
+
+    if (next) {
+        next.style.animation = `slideOut${direction === "left" ? "Left" : "Right"} 0.5s`;
+
+        // Transition
+        setTimeout(() => {
+            next.style.animation = "";
+            next.style.display = "none";
+            current.style.animation = "";
+        }, 500);
+    }
+
+    console.log(n);
 }
