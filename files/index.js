48d58271b0620662b4417d2b3ae15783629f0324
Jaka Černetič <jaka.cernetic@students.vegova.si>
2023-11-26 21:52:16 +0100

    neki3

--- files/index.js
+++ files/index.js
@@ -26,3 +26,11 @@ function scrollToTop() {
         behavior: 'smooth'
     });
 }
+
+function switchTheme() {
+    switcher = document.querySelector('.switcher');
+    document.body.classList.toggle('dark');
+    document.body.classList.toggle('light');
+    switcher.classList.toggle('fa-moon');
+    switcher.classList.toggle('fa-sun');
+}
--- files/switcher.js
+++
@@ -1,7 +0,0 @@
-function switchTheme() {
-    switcher = document.querySelector('.switcher');
-    document.body.classList.toggle('dark');
-    document.body.classList.toggle('light');
-    switcher.classList.toggle('fa-moon');
-    switcher.classList.toggle('fa-sun');
-}
