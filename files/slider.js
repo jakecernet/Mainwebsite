ece7f92ea7e5abe7c6fc12c3ba924bac303f193b
Jaka Černetič <jaka.cernetic@students.vegova.si>
2023-12-28 18:33:33 +0100

    neki

--- files/slider.js
+++ files/slider.js
@@ -1,6 +1,6 @@
 let n = 1;
 
-for (let i = 1; i < 8; i++) {
+for (let i = 1; i < 4; i++) {
     const slide = document.getElementById(i);
     slide.style.opacity = "0";
     slide.style.display = "none";
@@ -13,9 +13,9 @@ function moveSlides(direction) {
     const previousN = n;
 
     if (direction === "left") {
-        n = (n % 7) + 1;
+        n = (n % 3) + 1;
     } else {
-        n = (n - 2 + 7) % 7 + 1;
+        n = (n - 2 + 3) % 3 + 1;
     }
 
     const currentSlide = document.getElementById(n);
@@ -35,7 +35,7 @@ function moveSlides(direction) {
 
     // If going back to the first slide, hide the last slide
     if (n === 1 && direction === "left") {
-        const lastSlide = document.getElementById(7);
+        const lastSlide = document.getElementById(3);
         if (lastSlide) {
             lastSlide.style.opacity = "0";
             lastSlide.style.zIndex = "0";
